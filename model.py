957541ba53746950736770f5bc54b0c35269e797
Rustamova Malakkhanim <70220517+Malakkhanim@users.noreply.github.com>
2023-08-22 10:38:26 -0700

    model.py

--- model.py
+++ model.py
@@ -9,29 +9,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-import warnings
-pd.set_option('display.max_column',None)
-warnings.filterwarnings('ignore')
-
-
-# In[2]:
-
 
 df = pd.read_csv('weather.csv')
 
-
-# In[3]:
-
-
 col_names = df.columns
-
-
-# In[4]:
-
-
-# find categorical variables
 
 categorical = [var for var in df.columns if df[var].dtype=='O']
 
@@ -40,23 +21,10 @@ print('There are {} categorical variables\n'.format(len(categorical)))
 print('The categorical variables are :', categorical)
 
 
-# In[5]:
-
-
-# print categorical variables containing missing values
 
 cat1 = [var for var in categorical if df[var].isnull().sum()!=0]
 
-
-# We can see that there are only 4 categorical variables in the dataset which contains missing values. These are `WindGustDir`, `WindDir9am`, `WindDir3pm` and `RainToday`.
-
-# In[6]:
-
-
 df.drop('Date', axis=1, inplace = True)
-
-
-# In[7]:
 
 
 def encode(data, col, max_val):
@@ -64,41 +32,22 @@ def encode(data, col, max_val):
     data[col + '_cos'] = np.cos(2 * np.pi * data[col]/max_val)
     return data
 
-
-# In[8]:
-
-
-# find outliers for Rainfall variable
-
 IQR = df.Rainfall.quantile(0.75) - df.Rainfall.quantile(0.25)
 Lower_fence = df.Rainfall.quantile(0.25) - (IQR * 3)
 Upper_fence = df.Rainfall.quantile(0.75) + (IQR * 3)
 
 
-# In[9]:
-
-
-# find outliers for Evaporation variable
 
 IQR = df.Evaporation.quantile(0.75) - df.Evaporation.quantile(0.25)
 Lower_fence = df.Evaporation.quantile(0.25) - (IQR * 3)
 Upper_fence = df.Evaporation.quantile(0.75) + (IQR * 3)
 
 
-# In[10]:
-
-
-# find outliers for WindSpeed9am variable
 
 IQR = df.WindSpeed9am.quantile(0.75) - df.WindSpeed9am.quantile(0.25)
 Lower_fence = df.WindSpeed9am.quantile(0.25) - (IQR * 3)
 Upper_fence = df.WindSpeed9am.quantile(0.75) + (IQR * 3)
 
-
-# In[11]:
-
-
-# find outliers for WindSpeed3pm variable
 
 IQR = df.WindSpeed3pm.quantile(0.75) - df.WindSpeed3pm.quantile(0.25)
 Lower_fence = df.WindSpeed3pm.quantile(0.25) - (IQR * 3)
@@ -113,43 +62,18 @@ X = df.drop(['RainTomorrow'], axis=1)
 y = df['RainTomorrow']
 
 
-# # **Split data into separate training and test set** 
-# 
-
-# In[13]:
-
-
-# split X and y into training and testing sets
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-
-# In[14]:
-
-
-# display categorical variables
-
 categorical = [col for col in X_train.columns if X_train[col].dtypes == 'O']
 
-
-# In[15]:
-
-
-# display numerical variables
 
 numerical = [col for col in X_train.columns if X_train[col].dtypes != 'O']
 
 
-# In[16]:
-
-
 numerical = [col for col in df.columns if df[col].dtypes != 'O']
-
-
-# In[17]:
-
 
 # Calculate information value
 def calc_iv(df, feature, target, pr=0):
@@ -179,9 +103,6 @@ def calc_iv(df, feature, target, pr=0):
     return data
 
 
-# In[18]:
-
-
 num_bins = 5  
 binned_data = df.copy() 
 
@@ -189,20 +110,9 @@ for column in numerical:
     
     binned_data[column + '_bin'] = pd.qcut(binned_data[column], num_bins, labels=False, duplicates='drop')
 
-
-# In[19]:
-
-
 binned_data.RainTomorrow = [0 if i=='No' else 1 for i in binned_data.RainTomorrow]
 
-
-# In[20]:
-
-
 cols = [col for col in binned_data.columns if col.endswith('_bin')]
-
-
-# In[21]:
 
 
 for colname in cols:
@@ -216,19 +126,11 @@ for colname in cols:
            X_test.drop(colname[:-4],axis=1,inplace=True)
 
 
-# In[22]:
-
-
-# impute missing categorical variables with most frequent value
-
 for temp in [X_train, X_test]:
     temp['WindGustDir'].fillna(X_train['WindGustDir'].mode()[0], inplace=True)
     temp['WindDir9am'].fillna(X_train['WindDir9am'].mode()[0], inplace=True)
     temp['WindDir3pm'].fillna(X_train['WindDir3pm'].mode()[0], inplace=True)
     temp['RainToday'].fillna(X_train['RainToday'].mode()[0], inplace=True)
-
-
-# In[23]:
 
 
 def max_value(temp, variable, top):
@@ -240,26 +142,12 @@ for temp in [X_train, X_test]:
     temp['WindSpeed3pm'] = max_value(temp, 'WindSpeed3pm', 57)
 
 
-# In[24]:
-
-
 train = pd.get_dummies(X_train,drop_first='True')
-
-
-# In[25]:
-
 
 test = pd.get_dummies(X_test,drop_first='True')
 
 
-# In[26]:
-
-
 cols = train.columns
-
-
-# In[27]:
-
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -269,64 +157,27 @@ train = scaler.fit_transform(train)
 
 test = scaler.transform(test)
 
-
-# In[28]:
-
-
 train = pd.DataFrame(train, columns=[cols])
 
-
-# In[29]:
-
-
 test = pd.DataFrame(test, columns=[cols])
-
-
-# In[30]:
-
 
 y_train = [0 if i=='No' else 1 for i in y_train]
 y_test = [0 if i=='No' else 1 for i in y_test]
 
-
-# In[31]:
-
-
 # train a logistic regression model on the training set
 from sklearn.linear_model import LogisticRegression
-
-
-# instantiate the model
 logreg = LogisticRegression(solver='liblinear', random_state=0)
 
-
-# fit the model
 logreg.fit(train, y_train)
-
-
-# In[32]:
 
 
 y_pred_test = logreg.predict(test)
 
-
-# In[33]:
 
 
 from sklearn.metrics import *
 y_pred_test = logreg.predict(test)
 print('Model accuracy score: {0:0.4f}'. format(accuracy_score(y_test, y_pred_test)))
 
-
-# In[34]:
-
-
 with open('metrics.csv','w') as outfile:
     outfile.write('Model accuracy score: {0:0.4f}'.format(accuracy_score(y_test, y_pred_test)))
-
-
-# In[ ]:
-
-
-
-
